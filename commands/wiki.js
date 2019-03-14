1de89ec259c1c5b0df4d1595b9ac154b02a20f64
Justin Edmund <justin@jedmund.com>
2019-03-14 04:36:06 -0700

    Working on scraping character skills

--- commands/wiki.js
+++ commands/wiki.js
@@ -2,6 +2,7 @@ const { Command } = require('discord-akairo')
 const { RichEmbed } = require('discord.js')
 const DomParser = require('dom-parser')
 const fetch = require("node-fetch")
+const striptags = require("striptags")
 
 class WikiCommand extends Command {
     constructor() {
@@ -25,23 +26,52 @@ class WikiCommand extends Command {
     }
 
     exec(message, args) {
+        this.parser = new DomParser()
+
         const url = new URL('https://gbf.wiki/Shiva')
         fetch(url)
             .then((response) => {
                 return response.text()
             })
             .then((html) => {
-                var parser = new DomParser()
-
-                var dom = parser.parseFromString(html, "text/html")
+                console.log("Fetched URL!")
+                var dom = this.parser.parseFromString(html, "text/html")
                 var content = dom.getElementById('mw-content-text')
 
-                console.log(content.innerHTML)
+                this.getCharacterSkills(content)
             })
             .catch(function(error) {
                 console.log(error)
             })
 
+    }
+
+    getCharacterSkills(html) {
+        let tables = html.getElementsByClassName("character")
+
+        let skillsWrapper = tables[2]
+        let skillNames = skillsWrapper.getElementsByClassName("skill-name")
+        let rawSkills = skillNames.map(function(item) {
+            return item.parentNode
+        })
+
+        var skills = []
+
+        for (var i in rawSkills) {
+            var rawSkill = rawSkills[i]
+            var skill = {}
+
+            skill.name = rawSkill.getElementsByClassName("skill-name")[0].innerHTML.trim()
+            skill.icon = rawSkill.getElementsByClassName("skill-icon")[0].childNodes[1].getAttribute("src")
+            
+            let strippedDescription = striptags(rawSkill.childNodes[11].innerHTML.trim(), ['br'])
+            skill.description = striptags(strippedDescription, [], "\n")
+
+            console.log(skill.name)
+            console.log(skill.icon)
+            console.log(skill.description)
+            console.log("-----------------------------------------")
+        }
     }
 }
 
